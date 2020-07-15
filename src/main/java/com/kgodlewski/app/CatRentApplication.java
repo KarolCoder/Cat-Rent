732035f26cd9d025b3375333b0d4410b98c5eb19
KarolG <godlewski215@gmail.com>
2020-07-15 21:05:44 +0200

    First commit

---
+++ src/main/java/com/kgodlewski/app/CatRentApplication.java
@@ -0,0 +1,13 @@
+package com.kgodlewski.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatRentApplication.class, args);
+	}
+
+}
